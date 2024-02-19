135a77ba4b8d1c2f0bcba529308333dd77ebb748
SamahaAnwar <i200424@nu.edu.pk>
2024-02-19 09:14:23 +0500

    First Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class StudentsInMLOps:
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+        
+    def enrollStudents(self, roll_no):
+        self.strength += 1
+        self
+    def dropStudents(self, roll_no):
+        self.strength -= 1
+
+
+    def getTotalStrength(self):
+        return self.strength 
+                
+    def getClassName(self):
+        return "MLOps N"            
